b99c330f6049d6dabff87582d8077f6874948380
krupalidobariya <dobariyakrupali208@gmail.com>
2025-03-29 19:07:45 -0400

    Make sure the use of ACCESS_FINE_LOCATION permission is necessary.

--- app/src/main/java/de/tadris/fitness/activity/RecordWorkoutActivity.java
+++ app/src/main/java/de/tadris/fitness/activity/RecordWorkoutActivity.java
@@ -258,6 +258,12 @@ public class RecordWorkoutActivity extends FitoTrackActivity implements Location
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 10);
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 10);
         }
+
+        else {
+            startListener();  
+        }
+
+        
     }
 
     private boolean hasPermission() {
@@ -268,6 +274,14 @@ public class RecordWorkoutActivity extends FitoTrackActivity implements Location
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (hasPermission()) {
             startListener();
+        }
+
+        else {
+            new AlertDialog.Builder(this)
+                .setTitle("Permission Denied")
+                .setMessage("GPS permission is required to record the workout.")
+                .setPositiveButton("OK", (dialog, which) -> dialog.dismiss())
+                .show();
         }
     }
 
